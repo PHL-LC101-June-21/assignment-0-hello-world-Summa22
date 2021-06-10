18e7fd09f00c76f0db8479dab3192dda7ebbef83
Summer Kollie <85651919+Summa22@users.noreply.github.com>
2021-06-10 20:46:45 +0000

    I defined world and printed Hello World!

--- hello.js
+++ hello.js
@@ -2,5 +2,8 @@ function hello() {
   // TODO: change this string so that your program prints "Hello world!" 
   // when you hit the replit run button
   return "Hello!";
-}
+} let world = "World!"
+
+console.log('Hello World!'); 
+
 module.exports = hello;
